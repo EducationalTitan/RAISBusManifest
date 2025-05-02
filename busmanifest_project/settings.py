1716f7b5d06fa34e17e1ca530f39a4ebbd6f7850
Wilcox Kingsley Ala <111095726+EducationalTitan@users.noreply.github.com>
2025-05-02 12:21:24 +0100

    Update settings.py

--- busmanifest_project/settings.py
+++ busmanifest_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(e2uq0em*dqlw$(cwj0wap708y-a!cwb0^(1-(!&3dqic5m!k*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost','raisbusmanifest.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'raisbusmanifest.onrender.com']
 
 
 # Application definition
