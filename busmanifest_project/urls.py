50d4fcbd5078e1f70eb814cf3929871dbc9de1ac
Wilcox Kingsley Ala <111095726+EducationalTitan@users.noreply.github.com>
2025-05-02 12:24:14 +0100

    Update urls.py

--- busmanifest_project/urls.py
+++ busmanifest_project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from manifest.views import homepage_view
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', homepage_view, name='homepage'),
@@ -10,3 +12,6 @@ urlpatterns = [
     path('manifest/', include('manifest.urls')),  # Routes like /manifest/list/
     path('login/', auth_views.LoginView.as_view(template_name='manifest/login.html'), name='login'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
